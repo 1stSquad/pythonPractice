e3bf3f31b2b58ad1d9e9ab5a1e2f4cea5e792597
1stSquad <desnotu@yandex.ru>
2025-02-08 05:29:12 +0300

    7 дз исправлено очень умным наставником

--- 07_lesson/form_test.py
+++ 07_lesson/form_test.py
@@ -1,18 +1,54 @@
+# from selenium import webdriver
+# from Pages.from_page import FormPage
+#
+#
+# def test_complete_the_form():
+#     # Инициализация драйвера
+#     driver = webdriver.Chrome()
+#
+#     # Создание экземпляра страницы
+#     form_page = FormPage(driver)
+#
+#     # Выполнение тестовых действий
+#     form_page.complete_the_form()
+#     form_page.sublime_click()
+#     form_page.zip_code_red()
+#
+#     # Закрытие браузера
+#     driver.quit()
+
+import pytest
 from selenium import webdriver
 from Pages.from_page import FormPage
 
 
-def test_complete_the_form():
-    # Инициализация драйвера
-    driver = webdriver.Chrome()
+@pytest.fixture()
+def driver():
+    browser = webdriver.Chrome()
+    yield browser
+    browser.quit()
 
-    # Создание экземпляра страницы
+
+def test_form_input(driver):
     form_page = FormPage(driver)
+    form_page.input_value("first-name", 'Иван')
+    form_page.input_value('last-name', 'Петров')
+    form_page.input_value('address', 'Ленина, 55-3')
+    form_page.input_value('e-mail', 'test@skypro.com')
+    form_page.input_value('phone', '+7985899998787')
+    form_page.input_value('city', 'Москва')
+    form_page.input_value('country', 'Россия')
+    form_page.input_value('job-position', 'QA')
+    form_page.input_value('company', 'SkyPro')
 
-    # Выполнение тестовых действий
-    form_page.complete_the_form()
-    form_page.sublime_click()
-    form_page.zip_code_red()
+    form_page.button_click()
 
-    # Закрытие браузера
-    driver.quit()
+    id_elements = ['#first-name', '#last-name', '#address', '#e-mail', '#phone', '#city', '#country', '#job-position',
+                   '#company']
+
+    for id_element in id_elements:
+        color_field = form_page.checking_input(id_element)
+        assert color_field == 'rgba(209, 231, 221, 1)'
+    color_zip_code = form_page.checking_empty_input('#zip-code')
+
+    assert color_zip_code == 'rgba(248, 215, 218, 1)'
