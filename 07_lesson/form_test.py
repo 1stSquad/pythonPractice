baf515f07c98e1ff60beb080d57a25615a0226e9
1stSquad <desnotu@yandex.ru>
2025-02-08 05:29:36 +0300

    7 дз исправлено очень умным наставником!!!

--- 07_lesson/form_test.py
+++ 07_lesson/form_test.py
@@ -1,22 +1,3 @@
-# from selenium import webdriver
-# from Pages.from_page import FormPage
-#
-#
-# def test_complete_the_form():
-#     # Инициализация драйвера
-#     driver = webdriver.Chrome()
-#
-#     # Создание экземпляра страницы
-#     form_page = FormPage(driver)
-#
-#     # Выполнение тестовых действий
-#     form_page.complete_the_form()
-#     form_page.sublime_click()
-#     form_page.zip_code_red()
-#
-#     # Закрытие браузера
-#     driver.quit()
-
 import pytest
 from selenium import webdriver
 from Pages.from_page import FormPage
